798937efae904e124fce12f153d45e2d18e53130
swapfile3 <swajeetg768@gmail.com>
2020-11-13 17:53:08 +0530

    Test background

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import NavBar from './Components/NavBar'
+import BackgroundCover from './Components/BackgroundCover'
 
 const Landing = () => {
   return (
     <div>
+      
       <NavBar />
+      <BackgroundCover />
     </div>
   )
 }
