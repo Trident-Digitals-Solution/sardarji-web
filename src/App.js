8f8be37131ec333ec7acaaff5c0bc1a3f93bde6e
SonuKumar81800 <sonu.patna0808@gmail.com>
2020-11-15 14:26:34 +0530

    Router in Footer Section Routing property is added in the Footer Section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 // import './App.css';
 import Landing from './components/Landing/Landing';
-// import { Route, Router, Switch } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 // import Loading from './components/Loding';
 
 function App() {
   return (
     <>
       {/* <Loading /> */}
-      <Landing />
+      <Router>
+
+        <Landing />
+      </Router>
     </>
   );
 }
